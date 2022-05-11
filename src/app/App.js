e7cea9770c89b442f1cc5f07f61582b2153f5a32
malamsyah <hi@malamsyah.com>
2022-05-11 22:46:56 +0700

    Add authed user check on app

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { connect } from "react-redux";
 import Navigation from "../components/Navigation";
+import { handleInitialData } from "../actions/index";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../components/LoginPage";
 import HomePage from "../components/HomePage";
 import LeaderboardPage from "../components/LeaderboardPage";
 import NewPollPage from "../components/NewPollPage";
+import LoadingBar from "react-redux-loading-bar";
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    if (props.authedUser === null) {
+      return;
+    }
+
+    props.dispatch(handleInitialData());
+  }, [props, props.authedUser]);
+
+  if (props.authedUser === null) {
+    return <LoginPage />;
+  }
+
   return (
     <div className="App">
       <Navigation />
+      <LoadingBar />
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} />
@@ -21,4 +38,8 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = ({ authedUser }) => ({
+  authedUser,
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer, middleware);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
