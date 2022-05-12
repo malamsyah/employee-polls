b847c763e3017de0721aaa87b7eb0e876424d789
malamsyah <hi@malamsyah.com>
2022-05-12 20:14:17 +0700

    group questions if logged in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,18 @@
 import { getInitialData } from "../utils/index";
 import { receiveUsers } from "./users";
-import { receiveQuestions } from "./questions";
+import { groupQuestions, receiveQuestions } from "./questions";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 
 export function handleInitialData() {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
+      const { authedUser } = getState();
+      if (authedUser != null) {
+        dispatch(groupQuestions(authedUser));
+      }
       dispatch(hideLoading());
     });
   };
--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,16 @@
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const GROUP_QUESTIONS = "GROUP_QUESTIONS";
 
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  };
+}
+
+export function groupQuestions(username) {
+  return {
+    type: GROUP_QUESTIONS,
+    username: username,
   };
 }
